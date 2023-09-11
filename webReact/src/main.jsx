0e87075a95e9ef636ee1a721c31bfeb12cbfedb8
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 <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>
2023-09-10 22:44:52 -0300

    UserWelcome

--- webReact/src/main.jsx
+++ webReact/src/main.jsx
@@ -2,9 +2,11 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
+import UserWelcome from './UserWelcome.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
+    <UserWelcome />
     <App />
   </React.StrictMode>,
 )
